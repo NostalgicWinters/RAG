b5b87f9097b19652339065e31a6ebf1b70665812
NostalgicWinters <Dev.Sterling@outlook.com>
2026-07-13 23:54:42 +0530

    Added retrieval pipeline

---
+++ retrieval_pipeline.py
@@ -0,0 +1,24 @@
+from re import search
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+persistent_directory = "db/chroma_db"
+
+embedding_model = OllamaEmbeddings(model="nomic-embed-text")
+
+db = Chroma(
+    persist_directory=persistent_directory,
+    embedding_function=embedding_model,
+    collection_metadata={"hnsw": "cosine"},
+)
+
+query = "Which island does SpaceX lease for it's launch in the Pacific?"
+
+retriever = db.as_retriever(search_kwargs={"k": 5})
+
+relevant_docs = retriever.invoke(query)
+
+print(f" User Query: {query}")
+print("--- Context ---")
+for i, doc in enumerate(relevant_docs, 1):
+    print(f"Document {i}: \n{doc.page_content}\n")
